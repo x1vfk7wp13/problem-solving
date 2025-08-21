88fa5d1318d46149d228d77a1ee436c38f9cac5e
unobatbayar <38866249+x1vfk7wp13@users.noreply.github.com>
2025-08-21 14:40:55 +0900

    solve: 20 Valid Parantheses

---
+++ LeetCode/20_valid_parantheses.py
@@ -0,0 +1,18 @@
+﻿class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        parantheses = {')': '(', '}':'{', ']':'['}
+
+        for c in s:
+            if c in parantheses.values():
+                # opening
+                stack.append(c)
+            else:
+                #closing
+                if len(stack) > 0 and stack[-1] == parantheses[c]:
+                    stack.pop()
+                else:
+                    return False
+
+        return len(stack) == 0
+                
